ba87fd322084cf439b5b4c9fd19bc49a87ea8ed5
tpreg <tamas.preg@gmail.com>
2024-02-11 08:37:39 +0100

    Initial commit

---
+++ src/main/java/com/kesmarki/personregistry/PersonregistryApplication.java
@@ -0,0 +1,13 @@
+package com.kesmarki.personregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonregistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kesmarki/personregistry/PersonregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kesmarki.personregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
