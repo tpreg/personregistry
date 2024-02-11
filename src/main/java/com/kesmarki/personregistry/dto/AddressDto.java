79925ef063b1e176bd35df784edf32012bfe0c71
tpreg <tamas.preg@gmail.com>
2024-02-11 14:16:57 +0100

    Extend DTOs with additional fields

--- src/main/java/com/kesmarki/personregistry/dto/AddressDto.java
+++ src/main/java/com/kesmarki/personregistry/dto/AddressDto.java
@@ -6,6 +6,6 @@ import java.util.UUID;
 /**
  * DTO for {@link com.kesmarki.personregistry.model.Address}
  */
-public record AddressDto(UUID id, List<ContactDto> contacts) {
+public record AddressDto(UUID id, String country, String zipCode, String city, String street, List<ContactDto> contacts) {
 
 }
--- src/main/java/com/kesmarki/personregistry/dto/PersonDto.java
+++ src/main/java/com/kesmarki/personregistry/dto/PersonDto.java
@@ -1,9 +1,12 @@
 package com.kesmarki.personregistry.dto;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 /**
  * DTO for {@link com.kesmarki.personregistry.model.Person}
  */
-public record PersonDto(UUID id, AddressDto physicalAddress, AddressDto residentialAddress) {
+public record PersonDto(UUID id, String fullName, LocalDate dateOfBirth, String birthplace, AddressDto physicalAddress,
+                        AddressDto residentialAddress
+) {
 }
