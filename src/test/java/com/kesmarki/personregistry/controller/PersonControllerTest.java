8e7d8bd997213673eb585c84b0e05a4ba9c3659a
tpreg <tamas.preg@gmail.com>
2024-02-11 14:51:48 +0100

    Fix PersonController test

--- src/test/java/com/kesmarki/personregistry/controller/PersonControllerTest.java
+++ src/test/java/com/kesmarki/personregistry/controller/PersonControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 import static org.mockito.BDDMockito.given;
@@ -37,8 +38,8 @@ public class PersonControllerTest {
 
 	@Test
 	public void savePerson_ValidData_Success() throws Exception {
-		final var inputDto = new PersonDto(UUID.fromString("b83e3e2b-a005-4116-a0d4-8f9d93c80f10"), null, null);
-		final var outputDto = new PersonDto(UUID.fromString("b83e3e2b-a005-4116-a0d4-8f9d93c80f10"), null, null);
+		final var inputDto = new PersonDto(UUID.randomUUID(), "Cserepes Virág", LocalDate.of(1991, 10, 2), "Hungary", null, null);
+		final var outputDto = new PersonDto(UUID.randomUUID(), "Cserepes Virág", LocalDate.of(1991, 10, 2), "Hungary", null, null);
 		given(this.personService.save(ArgumentMatchers.any(PersonDto.class))).willReturn(outputDto);
 		this.mockMvc.perform(post("/persons/")
 						.contentType(APPLICATION_JSON)
